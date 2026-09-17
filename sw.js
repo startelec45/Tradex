8329f1f2f3aed9ca281dd05f102554b0e46df494
startelec45 <startelec45@gmail.com>
2026-09-17 20:23:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tradex-v3';
+const CACHE = 'tradex-v3.1';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
